c470200b1b39c2b26c494be81ae490f40040b9bd
Eric Woudstra <ericwouds@gmail.com>
2025-01-24 16:09:03 +0100

    netfilter: flow: Add bridge_vid member
    Store the vid used on the bridge in the flow_offload_tuple, so it can be
    used later to identify fdb entries that relate to the tuple.
    
    The bridge_vid member is added to the structures nft_forward_info,
    nf_flow_route and flow_offload_tuple. It can now be passed from
    net_device_path->bridge.vlan_id to flow_offload_tuple->out.bridge_vid.

--- net/netfilter/nft_flow_offload.c
+++ net/netfilter/nft_flow_offload.c
@@ -84,6 +84,7 @@ struct nft_forward_info {
 		__u16	id;
 		__be16	proto;
 	} encap[NF_FLOW_TABLE_ENCAP_MAX];
+	u16 bridge_vid;
 	u8 num_encaps;
 	u8 ingress_vlans;
 	u8 h_source[ETH_ALEN];
@@ -159,6 +160,7 @@ static void nft_dev_path_info(const struct net_device_path_stack *stack,
 			case DEV_PATH_BR_VLAN_KEEP:
 				break;
 			}
+			info->bridge_vid = path->bridge.vlan_id;
 			info->xmit_type = FLOW_OFFLOAD_XMIT_DIRECT;
 			break;
 		default:
@@ -219,6 +221,7 @@ static void nft_dev_forward_path(struct nf_flow_route *route,
 		memcpy(route->tuple[dir].out.h_source, info.h_source, ETH_ALEN);
 		memcpy(route->tuple[dir].out.h_dest, info.h_dest, ETH_ALEN);
 		route->tuple[dir].out.ifindex = info.outdev->ifindex;
+		route->tuple[dir].out.bridge_vid = info.bridge_vid;
 		route->tuple[dir].xmit_type = info.xmit_type;
 	}
 }
